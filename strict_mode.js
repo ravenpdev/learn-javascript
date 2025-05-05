44af539052c1ddb1a358db96745f5fad2c0330d5
Raven Paragas <ravenparagas@Ravens-MacBook-Pro.local>
2025-05-05 14:17:04 +0800

    chore: 'use strict' strict mode

---
+++ strict_mode.js
@@ -0,0 +1,18 @@
+"use strict";
+// The modernn mode, "use strict"
+//
+// "use strict"
+//
+// The directive looks like a string "use strict" or 'use strict'. When it is located at the top of a script, the whole script works the "modern" way.
+//
+// "use strict" can be put at the beginnning of a function. Doing that enables strict mode in that function only. But usually people use it for the whole script.
+//
+// There's no way to cancel use strict
+// There is no directive lie "no use strict" that reverts the engine to old behavior.
+// Once we enter strict mode, there's no going back.
+//
+//
+// Should we "use strict"?
+//
+// One could recommend to start scripts with "use strict"
+// Modern JavaScript supports "classes" and "modules" - advanced language structures that enable use strict automatically. So we don't need to add the "use strict" directive, if we use them.
