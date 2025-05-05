1f1cb7bb0fdcc872494c5b4186f504e7f9c05bf3
Raven Paragas <ravenparagas@Ravens-MacBook-Pro.local>
2025-05-05 14:06:51 +0800

    chore: semicolon and comments

---
+++ structure.js
@@ -0,0 +1,12 @@
+console.log("hello");
+
+[1, 2].forEach((e) => console.log(e));
+
+[1, 2].forEach(console.log);
+
+// This comment occupies a linenn of its own
+console.log("Hello");
+console.log("World");
+
+/* this is a multi line
+comment!!. */
